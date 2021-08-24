34560cf9753aa1ffcab8368c7530d7b5447876ae
Angela Loftus <angelaloftus@yahoo.com>
2021-08-23 23:08:33 -0500

    refactored add task item with object

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,44 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
 
 //function to create a new task on list
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     //prevents page from automatically refreshing 
     event.preventDefault();
 
-    //adds new item to list and styles it same as others
+    var taskNameInput = document.querySelector("input[name ='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name = 'task-type']").value;
+
+    //package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    //send it as an argument to createTaskE1
+    createTaskE1(taskDataObj);
+    
+}
+
+//creating a new function
+var createTaskE1 = function(taskDataObj) {
+    //create list item
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    //give it a class name
+    taskInfoE1.className = "task-info";
+    //add HTML content to div
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class = 'task-type'>" + taskDataObj.type + "</span>";
+
+    listItemE1.appendChild(taskInfoE1);
+
+    //add entire list item to list
     tasksToDoE1.appendChild(listItemE1);
 }
 
+
 //listens for the user to submit the form (submit listener)
-formE1.addEventListener("submit", createTaskHandler);
+formE1.addEventListener("submit", taskFormHandler);
 
