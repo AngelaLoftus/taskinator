a12b118d4b041cbd5e49facdd7b004320857bac5
Angela Loftus <angelaloftus@yahoo.com>
2021-08-23 22:31:24 -0500

    add submit listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
 
 //function to create a new task on list
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    //prevents page from automatically refreshing 
+    event.preventDefault();
+
+    //adds new item to list and styles it same as others
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task.";
     tasksToDoE1.appendChild(listItemE1);
 }
 
-//this statement can be read, "on a button click, create task"
-buttonE1.addEventListener("click", createTaskHandler);
+//listens for the user to submit the form (submit listener)
+formE1.addEventListener("submit", createTaskHandler);
 
